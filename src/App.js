4e639b1bec196a3005ba4b01993a9c74894790b5
kemallaydn <kemal.aydin3429@gmail.com>
2025-05-08 14:41:44 +0300

    aaaa

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   // Evet butonuna tıklandığında alert göster
   const handleYesClick = () => {
-    alert("Aşkım, ben de seni seviyorum!"); // Evet butonuna tıklandığında gösterilecek mesaj
+    alert("Aşkım, ben de seni seviyorum!!"); // Evet butonuna tıklandığında gösterilecek mesaj
   };
 
   return (
