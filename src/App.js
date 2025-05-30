b02ddf8bed62da99cdb465fa3b77b56556ab0137
kemallaydn <kemal.aydin3429@gmail.com>
2025-05-30 20:03:26 +0300

    END

--- src/App.js
+++ src/App.js
@@ -5,31 +5,30 @@ function App() {
   return (
     <div className="App" style={{ textAlign: 'center', paddingTop: '20px', position: 'relative', height: '100vh', backgroundColor: '#f0f8ff' }}>
       <Typography variant="h4" gutterBottom style={{ color: '#6a5acd', fontFamily: 'Cursive' }}>
-        Merhaba Açelya,
+        Sevgili Açelya,
       </Typography>
       <Typography variant="h6" gutterBottom style={{ color: '#6a5acd', fontFamily: 'Cursive' }}>
-        Yaşadığmız onca güzel günden sonra böyle kötü ayrılmanın ikimiz için de çok zor olduğunu biliyorum.
-      </Typography>
-      <Typography variant="h6" gutterBottom style={{ color: '#6a5acd', fontFamily: 'Cursive' }}>
-        Sana son kez güzel bir sekilde veda etmek ve eşyalarını teslim etmek istiyorum.
-      </Typography>
-      <Typography variant="h6" gutterBottom style={{ color: '#6a5acd', fontFamily: 'Cursive' }}>
-         Lütfen yarın saat 10:00'da antik bahçenin oraya gelir misin?
+          Sana olan duygularımın bu kadar yoğun yaşamanın pişmanlığı ile yazıyorum bu sözleri.
+          Senle beraber cok guzel anılar biriktirdik, ama şimdi tüm bu anılar cok güzel bir hikayeye dönüşüyor.
+          Bana hep eskisi gibi biri olmadığımı söyledin. haklıydın değişmiştim. Olmadığım birine dönüştüm zamanla farkındayım.
+          Kıskanç, bencil, çekilmez, arkadaşlarınla arana giren, kendi iç çatışmalarını sana yansıtan birine dönüştüm. Hiç bir zaman olmadıgım ama nasıl olduğunu bilmediğim birine.
+          Sana olan aşkım (bunu bir bahane olarak sunmuyorum) beni tanıyamadığın biri yaptı.
+          Belkide  sebebi seni kaybetme korkumdu. Ama şimdi seni kaybettim.
+          Bu sözleri yazarken seni üzmenin acısıyla yazıyorum(belki de hiç dokunmicak kalbine. umarım öyle olur). Kötü bir sevgili oldum farkındayım. Bu yüzden seni suçlayamam.
+          Son buluşmamızda kötü ayrılmak belki de isteyeceğim son anlardandı. Kızgındım ama kendime minik bir kız çocuguna bunu nasıl yapabildim diye suçladım kendimi.
+          Beni affetmen için değil bu sözler artık çok geç farkındayım ama seni sevdiğimi bilmeni istiyorum.
+          Tek derdi o güzel yüzüne gülücek kondurmak olan bir cocugun, o gözlere sadece gözyaşı ile dokunmasının acısını keske bu sözlerle telafi edebilecek olsam.
+          Umarım bir gün seni hep güldürebilecek o güzel insanı bulursun. Bunu tüm kalbimle söylüyorum. Tek dileğim mutlu olman.
+
+         
+
+
+
       </Typography>
       <Typography variant="h6" style={{ color: '#6a5acd', fontFamily: 'Cursive' }}>
-          Seni seviyorum ❤️
+            Seni çok seven ve hep sevecek olan, FKA ❤️.
         </Typography>
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-        flexDirection: 'column',
-      }}>
-        <Typography variant="h6" style={{ color: '#6a5acd', fontFamily: 'Cursive' }}>
-          Bu mesajı görürsen lütfen herhangi bir yerden ulaşmaya çalışır mısın.
-        </Typography>
-      </Box>
+      
     </div>
   );
 }
